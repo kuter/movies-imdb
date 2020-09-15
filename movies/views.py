550172fe3cec14366217b7d48664278df3b60d71
kuter <contact@devktr.pl>
2020-09-15 13:26:45 +0200

    :bug: change serializer

--- movies/views.py
+++ movies/views.py
@@ -11,28 +11,23 @@ class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieAPISerializer
     filterset_class = MovieFilter
-    create_serializer_class = CreateMovieSerializer
+    serializer_class = CreateMovieSerializer
     api = OMDbAPI
 
-    def get_serializer_class(self):
-        if self.request.method == 'POST':
-            return self.create_serializer_class
-        return super().get_serializer_class()
-
     def create(self, request, *args, **kwargs):
-        api_serializer = self.get_serializer(data=request.data)
-        api_serializer.is_valid(raise_exception=True)
-        serializer = self.perform_create(api_serializer)
-        headers = self.get_success_headers(serializer.data)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        api_serializer = self.perform_create(serializer)
+        headers = self.get_success_headers(api_serializer.data)
         return Response(
-            serializer.validated_data,
+            api_serializer.validated_data,
             status=status.HTTP_201_CREATED,
             headers=headers,
         )
 
     def perform_create(self, serializer):
-        obj = self.api.get_movie_by_title(**serializer.validated_data)
-        serializer = self.serializer_class(data=obj)
+        obj = self.api.get_movie_by_title(serializer.validated_data['title'])
+        serializer = MovieAPISerializer(data=obj)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return serializer
